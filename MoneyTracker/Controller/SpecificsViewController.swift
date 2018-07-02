f0abf55828ad2c96bbf703298801a2d60b87d2be
Sushen <sat0006@jmss.vic.edu.au>
2018-07-02 14:22:54 +1000

    finishing first version of specifics screen, adding logic for changing money labels and width of progress bars

--- MoneyTracker/Controller/SpecificsViewController.swift
+++ MoneyTracker/Controller/SpecificsViewController.swift
@@ -9,11 +9,45 @@
 import UIKit
 
 class SpecificsViewController: UIViewController {
-
+    
+    // MARK: - user interface variables
+    
+    @IBOutlet var accountLables: [UILabel]!
+    @IBOutlet var moneyLables: [UILabel]!
+    
+    
+    
+    @IBOutlet var progressBars: [UIView]!
+    @IBOutlet weak var progressBarBackground: UIView!
+    @IBOutlet var progressBarWidthArray: [NSLayoutConstraint]!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //set initial values of accounts
+        accountLables[0].text = "CAH"
+        accountLables[1].text = "USD"
+        accountLables[2].text = "CSH"
+        accountLables[3].text = "ACC"
+        
+        
+        
+        for item in 0...progressBars.count {
+            progressBarWidthArray[item].constant = 0
+            //setWidthOfBar(sectionNo: item, setWidth: 0)
+        }
+        
+       
+        
+        
+        //self.view.layoutIfNeeded()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +55,48 @@ class SpecificsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    @IBAction func accountButtonPressed(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "goToChangeScreen", sender: self)
+        
+        
+    }
+    
+    
+    //MARK: - preparing for segues
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "goToChangeScreen") {
+            //set up preperation here
+            
+            let changeScreenViewController = segue.destination //...
+            
+            
+        }
+    }
+    
+    
+    
+    
+    // set width of the progress bar for a particular section
+    func setWidthOfBar(sectionNo : Int, setWidth : Double) {
+        
+        //section numbers start from 1 to end
+        progressBarWidthArray[sectionNo - 1].constant = CGFloat(setWidth)
+        self.view.layoutIfNeeded()
+        
+    }
+    
+    func setMoneyLable(sectionNo : Int, amount : Int) {
+        if(sectionNo >= 1 && sectionNo <= 4) {
+        let moneyLable = moneyLables[sectionNo - 1]
+        
+        moneyLable.text = "$\(amount)"
+            
+        }
+        
+    }
+ 
 
     /*
     // MARK: - Navigation
